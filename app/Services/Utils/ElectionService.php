47a38f81c37b9158e9fad5868850b94c382476c3
Ye Thi Ha Htwe <yethihahtwe319@gmail.com>
2025-03-13 08:28:57 +0000

    Thu, Mar 13, 2025, 3:28 PM +07:00

---
+++ app/Services/Utils/ElectionService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Utils;
+
+use App\Models\Election;
+
+class ElectionService
+{
+    public function getActiveElection(): Election
+    {
+        return Election::where('is_active', true)
+            ->where('start_date', '<=', now())
+            ->where('end_date', '>=', now())
+            ->first();
+    }
+}
